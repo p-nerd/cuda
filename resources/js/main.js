27ea4aa8d3e590e4ce7d1b05c73d6f7c5fe777ed
shihab <shihabmahamud02@gmail.com>
2020-07-06 04:35:58 +0600

    Finally cuda-version-1.0

--- resources/js/main.js
+++ resources/js/main.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
 
 	//SMOOT SCROLL FOR IE/ EDGE/ SAFARI
 
-	$("a").on('click', function(event) {
+	$("a").on('click', function (event) {
 
 		if (this.hash !== "") {
 			event.preventDefault();
@@ -38,3 +38,35 @@ function openNav() {
 function closeNav() {
 	document.getElementById("myNav").style.width = "0%";
 }
+
+
+// start animated-circle 
+
+
+$('svg.radial-progress').each(function (index, value) {
+	$(this).find($('circle.complete')).removeAttr('style');
+});
+$(window).scroll(function () {
+	$('svg.radial-progress').each(function (index, value) {
+		// If svg.radial-progress is approximately 25% vertically into the window when scrolling from the top or the bottom
+		if (
+			$(window).scrollTop() > $(this).offset().top - ($(window).height() * 0.75) &&
+			$(window).scrollTop() < $(this).offset().top + $(this).height() - ($(window).height() * 0.25)
+		) {
+			// Get percentage of progress
+			percent = $(value).data('percentage');
+			// Get radius of the svg's circle.complete
+			radius = $(this).find($('circle.complete')).attr('r');
+			// Get circumference (2πr)
+			circumference = 2 * Math.PI * radius;
+			// Get stroke-dashoffset value based on the percentage of the circumference
+			strokeDashOffset = circumference - ((percent * circumference) / 100);
+			// Transition progress for 1.25 seconds
+			$(this).find($('circle.complete')).animate({
+				'stroke-dashoffset': strokeDashOffset
+			}, 1250);
+		}
+	});
+}).trigger('scroll');
+
+// end animated-circle 
